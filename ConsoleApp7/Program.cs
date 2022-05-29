92c0dc708f65a605753dd3317d304fd3ee2583cf
Евгений Ташогло <tashoglo00@mail.ru>
2022-05-29 15:38:49 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = { -1,-2,-3,-4,0,5,6,7,8,9};
+var r1 = numbers.Where(s =>s<=0).ToList();
+foreach (var item in r1)
+{
+    Console.WriteLine(item);
+}
+var r2 = numbers.Where(s => s > 0);
+foreach (var item in r2)
+{
+    Console.WriteLine(item);
+}
